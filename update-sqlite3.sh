99179188ddda52881b8cbc29fab7f757b3547145
hityne <89051023+hityne@users.noreply.github.com>
2021-08-17 09:28:39 +0800

    Update and rename sqlite3-update.sh to update-sqlite3.sh

--- update-sqlite3.sh
+++ update-sqlite3.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
+echo "Sqlite3 will be updated to a new version."
+echo "Current Version："
+sqlite3 -version
+
+pause
+
 yum install -y gcc
 wget https://www.sqlite.org/2021/sqlite-autoconf-3350500.tar.gz
 tar zxvf sqlite-autoconf-3350500.tar.gz
 cd sqlite-autoconf-3350500/
 ./configure --prefix=/usr/local
 make && make install
-echo "sqlite3升级完成！"
-echo "当前版本号："
+echo "sqlite3 update is completed！"
+echo "New Version："
 sqlite3 -version
