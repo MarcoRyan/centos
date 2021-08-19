7059122c5275ba8e47b4dd6a44de512b9eb2f00f
hityne <89051023+hityne@users.noreply.github.com>
2021-08-19 15:51:50 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -35,7 +35,24 @@ elif [ "$main_no" = "2" ]; then
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove
 
 elif [ "$main_no" = "3" ]; then
-cat /usr/local/etc/v2ray/config.json
+ip=$(curl -s https://ipinfo.io/ip)
+echo "服务器地址：$ip"
+port=$(awk 'NR==3 {print $2}' /usr/local/etc/v2ray/config.json)
+port=${port:0:-1}
+echo "端口：$port"
+userid=$(awk 'NR==8 {print$2}' /usr/local/etc/v2ray/config.json)
+userid=${userid:1:-2}
+echo "用户id: $userid"
+alterid=$(awk 'NR==10 {print$2}' /usr/local/etc/v2ray/config.json)
+echo "额外id: $alterid"
+echo "传输协议：tcp"
+echo "伪装类型：none"
+echo "伪装域名："
+echo "伪装路径："
+echo "底层传输协议："
+
+if [ "$main_no" = "4" ]; then
+vim /usr/local/etc/v2ray/config.json
 
 else
 exit 0
